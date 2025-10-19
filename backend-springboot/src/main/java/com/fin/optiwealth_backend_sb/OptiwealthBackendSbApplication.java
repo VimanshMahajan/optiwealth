dcb1a1938967d9ba1875fbc2d7c9bbfa916a7c1c
Vimansh Mahajan <vimansh22572@iiitd.ac.in>
2025-10-19 23:50:09 +0530

    (backend-sb): setup structure

---
+++ backend-springboot/src/main/java/com/fin/optiwealth_backend_sb/OptiwealthBackendSbApplication.java
@@ -0,0 +1,13 @@
+package com.fin.optiwealth_backend_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptiwealthBackendSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptiwealthBackendSbApplication.class, args);
+	}
+
+}
---
+++ backend-springboot/src/test/java/com/fin/optiwealth_backend_sb/OptiwealthBackendSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fin.optiwealth_backend_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiwealthBackendSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
