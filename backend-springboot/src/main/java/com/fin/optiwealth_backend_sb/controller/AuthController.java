30aa554b22333cc20e08d1f68bbd2c7e1599b2bd
Vimansh Mahajan <vimansh22572@iiitd.ac.in>
2025-10-22 02:09:30 +0530

    Updated login functionality with JWT methods

--- backend-springboot/src/main/java/com/fin/optiwealth_backend_sb/controller/AuthController.java
+++ backend-springboot/src/main/java/com/fin/optiwealth_backend_sb/controller/AuthController.java
@@ -3,11 +3,14 @@ package com.fin.optiwealth_backend_sb.controller;
 import com.fin.optiwealth_backend_sb.dto.LoginClassDto;
 import com.fin.optiwealth_backend_sb.dto.UserRegistrationDto;
 import com.fin.optiwealth_backend_sb.dto.UserResponseDto;
+import com.fin.optiwealth_backend_sb.security.JwtService;
 import com.fin.optiwealth_backend_sb.service.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -15,6 +18,8 @@ public class AuthController {
 
     @Autowired
     private AppUserService appUserService;
+    @Autowired
+    private JwtService jwtService;
 
     @PostMapping("/register")
     public ResponseEntity<UserResponseDto> register(@Valid @RequestBody UserRegistrationDto dto) {
@@ -32,10 +37,18 @@ public class AuthController {
     public ResponseEntity<?> login(@RequestBody LoginClassDto dto) {
         try {
             UserResponseDto login = appUserService.login(dto);
-            return ResponseEntity.ok(login);
+
+            // generate JWT for authenticated user
+            String token = jwtService.generateToken(login.getEmail());
+
+            return ResponseEntity.ok(Map.of(
+                    "user", login,
+                    "token", token
+            ));
+
         } catch (RuntimeException e) {
             return ResponseEntity.status(401).body(e.getMessage());
         }
-
     }
+
 }
